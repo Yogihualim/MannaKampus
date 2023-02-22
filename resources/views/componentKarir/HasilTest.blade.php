aa4547f92fced6e5e0ba1db541fb09bb87ae570f
Yogi Hua <yogihualim@gmail.com>
2023-02-22 11:37:44 +0700

    22-2-2023

--- resources/views/componentKarir/HasilTest.blade.php
+++ resources/views/componentKarir/HasilTest.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <br>
-    <iframe src="{{ env("APP_URL")."/public/InformasiCabang.pdf" }}" frameborder="10" class="justify-content-center" width="100%" height="95%"></iframe>
+    <iframe src="{{ env("APP_URL")."/public/HasilTest.pdf" }}" frameborder="10" class="justify-content-center" width="100%" height="95%"></iframe>
     <br><br><br><br><br><br>
     <br><br><br><br><br><br>
     <br><br><br><br><br><br>
--- resources/views/cssStyle/mainPageCSS.blade.php
+++ resources/views/cssStyle/mainPageCSS.blade.php
@@ -104,6 +104,7 @@
         }
 
         .fa-instagram {
+            color: white;
             background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%); 
             background: -webkit-radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);
         }
