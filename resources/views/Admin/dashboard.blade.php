0063130d0bff4c70e4d0f134c072e7405fff10d7
Yogi Hua <yogihualim@gmail.com>
2023-03-09 15:07:20 +0700

    9-3-2023

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -14,7 +14,9 @@
     <div class="d-flex" id="wrapper">
         <!-- Sidebar-->
         <div class="border-end bg-white" id="sidebar-wrapper">
-            <div class="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
+            <div class="sidebar-heading border-bottom bg-light">
+                Start Bootstrap
+            </div>
             <div class="list-group list-group-flush">
                 <a class="list-group-item list-group-item-action list-group-item-light p-3" href="admin">Profil</a>
                 <a class="list-group-item list-group-item-action list-group-item-light p-3" href="katalog_jumat">Katalog Jumat</a>
